3528424901a255aa1d61ee97a4604689c68f84cc
lsan07 <5221@holbertonschool.com>
2022-06-15 18:18:35 -0500

    a

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -7,10 +7,10 @@
 */
 int main(void)
 {
-printf("%lu\n", sizeof(char));
-printf("%lu\n", sizeof(int));
-printf("%lu\n", sizeof(long int));
-printf("%lu\n", sizeof(long long));
-printf("%lu", sizeof(float));
+printf("Size of a char: %d byte(s)\n", sizeof(char));
+printf("Size of an int: %d byte(s)\n", sizeof(int));
+printf("Size of a long int: %d byte(s)\n", sizeof(long int));
+printf("Size of a long long int: %d byte(s)\n", sizeof(long long int);
+printf("Size of a float: %d byte(s)", sizeof(float));
 return (0);
 }
