ca71b4cb27a207fe334bcbd2b2f0ae64dc7ca1ab
lsan07 <5221@holbertonschool.com>
2022-06-16 21:24:07 -0500

    a

---
+++ variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ *main - rand find the number aleatory
+ *
+ *Return: 0
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+if (n < 0)
+	printf("%i is negative\n", n);
+else
+	if (n == 0)
+		printf("%i is zero\n", n);
+else
+	if (n > 0)
+		printf("%i is positive\n", n);
+	return (0);
+}
